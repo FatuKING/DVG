787840129499c432dfe210b2ce837a395c6947d3
Facundo Cozzani <cozzanifacundo@gmail.com>
2023-06-04 21:17:27 -0300

    Navbar terminado - DVG

---
+++ main.js
@@ -0,0 +1,7 @@
+// CSS
+import './style.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+// Script
+import 'bootstrap/dist/js/bootstrap.min.js'
